9081c49f82262cfe357a889c2ca83f6bca75fda1
dcanjura <diegoac053@gmail.com>
2026-05-09 22:59:20 -0600

    feat: Db context for project added

---
+++ SmartTaskAPI/Data/SmartTaskDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartTaskAPI.Models;
+
+namespace SmartTaskAPI.Data
+{
+    public class SmartTaskDbContext : DbContext
+    {
+        public SmartTaskDbContext(DbContextOptions<SmartTaskDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.TaskItem> TaskItems { get; set; } = null!;
+
+        public DbSet<Models.User> Users { get; set; } = null!;
+
+        public DbSet<Models.RefreshToken> RefreshTokens { get; set; } = null!;
+    }
+}
