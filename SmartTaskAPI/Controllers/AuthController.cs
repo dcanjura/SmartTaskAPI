387e2e86b15e1146b4186976cbebf03aa97fe965
Irvin <irvin.arguera4321@gmail.com>
2026-05-11 14:30:28 -0600

    feat: Adding controller implementation file

---
+++ SmartTaskAPI/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartTaskAPI.Services;
+
+namespace SmartTaskAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _service;
+
+        public AuthController(AuthService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            var result = _service.Register();
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            var result = _service.Login();
+            return Ok(result);
+        }
+
+        [HttpPost("refresh")]
+        public IActionResult RefreshToken()
+        {
+            var result = _service.RefreshToken();
+            return Ok(result);
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            _service.Logout();
+            return Ok("Logout successful");
+        }
+    }
+}
---
+++ SmartTaskAPI/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartTaskAPI.DTOs;
+using SmartTaskAPI.Services;
+
+namespace SmartTaskAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _service;
+
+        public UserController(UserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _service.GetUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _service.GetUserById(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] UserDTO userDto)
+        {
+            var user = _service.CreateUser(userDto);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] UserDTO userDto)
+        {
+            var user = _service.UpdateUser(id, userDto);
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            _service.DeleteUser(id);
+            return Ok();
+        }
+    }
+}
